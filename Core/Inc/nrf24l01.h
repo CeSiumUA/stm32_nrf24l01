f181b0bd62ddc3246f6cc4aeeb0d669598eec318
CeSiumUA <fedir.katushonok@gmail.com>
2024-05-09 23:02:45 +0300

    Added nrf files

---
+++ Core/Inc/nrf24l01.h
@@ -0,0 +1,17 @@
+/*
+ * nrf24l01.h
+ *
+ *  Created on: May 9, 2024
+ *      Author: mtgin
+ */
+
+#ifndef INC_NRF24L01_H_
+#define INC_NRF24L01_H_
+
+#include <stdint.h>
+
+struct nrf24l01_t {
+    
+};
+
+#endif /* INC_NRF24L01_H_ */
---
+++ Core/Src/nrf24l01.c
@@ -0,0 +1,9 @@
+/*
+ * nrf24l01.c
+ *
+ *  Created on: May 9, 2024
+ *      Author: mtgin
+ */
+
+
+#include "nrf24l01.h"
