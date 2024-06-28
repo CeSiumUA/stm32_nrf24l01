8be5b80d620fdffa4835cc97e83cd458ff4ef917
CeSiumUA <fedir.katushonok@gmail.com>
2024-06-28 23:28:32 +0300

    Added a script which replaces slashes

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ BUILD_DIR=build/${BUILD_CONFIGURATION}
 
 mkdir -p ${BUILD_DIR}
 
+if [ "$(uname)" != "MINGW"* ] && [ "$(uname)" != "CYGWIN"* ] && [ "$(uname)" != "MSYS"* ]; then
+  # Use sed to replace '\\' with '/' in the file cmake/st-project.cmake
+  sed -i 's#\\#/#g' cmake/st-project.cmake
+fi
+
 echo "Generating build files..."
 cmake -DCMAKE_BUILD_TYPE=${BUILD_CONFIGURATION} -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=cmake/gcc-arm-none-eabi.cmake -S . -B ${BUILD_DIR} -G Ninja
 
