e1d6ef79ab586a29068c99cfa8e2a6cb05415817
CeSiumUA <fedir.katushonok@gmail.com>
2024-07-01 19:03:06 +0300

    Working example

--- Core/Src/temperature.c
+++ Core/Src/temperature.c
@@ -10,13 +10,16 @@ float temperature_calculate(uint16_t *adc_value)
 {
     float temperature = 0.0f;
     float vdda, vref = 0.0f;
+    float vsense = 0.0f;
     uint32_t temp_sum = 0;
     uint32_t vref_sum = 0;
     uint16_t temp_avg, vref_avg = 0;
     for(int i = 0; i < (TEMPERATURE_ADC_BUFFER_SIZE / 4); i++)
     {
-        temp_sum += adc_value[i * 2];
-        vref_sum += adc_value[i * 2 + 1];
+        uint16_t tmp_val = adc_value[i * 2];
+        uint16_t vref_val = adc_value[i * 2 + 1];
+        temp_sum += (uint32_t)tmp_val;
+        vref_sum += (uint32_t)vref_val;
     }
 
     temp_avg = temp_sum / (TEMPERATURE_ADC_BUFFER_SIZE / 4);
@@ -24,9 +27,12 @@ float temperature_calculate(uint16_t *adc_value)
 
     vdda = (float)TEMPERATURE_TS_CAL_VREF * (float)(*TEMPERATURE_V_REF_INT_CAL) / (float)vref_avg / 1000.0f;
 
-    vref = (float)vdda / 4095 * vref_avg;
+    // vref = (float)vdda / 4095 * vref_avg;
 
-    temperature = (float)(((float)TEMPERATURE_TS_CAL2_TEMP - TEMPERATURE_TS_CAL1_TEMP) / (float)((*TEMPERATURE_TS_CAL2_ADDR) - (*TEMPERATURE_TS_CAL1_ADDR)) * (float)(temp_avg - (*TEMPERATURE_TS_CAL1_ADDR)) + TEMPERATURE_TS_CAL1_TEMP))));
+    vref = (float)((TEMPERATURE_V_REF_INT * 4095.0f)/vref_avg);
+    vsense = (float)((float)temp_avg * vref / 4095.0f);
+
+    temperature = (float)((vsense - TEMPERATURE_V_AT_25C) * 1000.0f) / TEMPERATURE_AVG_SLPOE + 25.0f;
 
     return temperature;
 }
