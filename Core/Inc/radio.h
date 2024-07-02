24d682b76380576b4f044133acd2473318f607d9
CeSiumUA <fedir.katushonok@gmail.com>
2024-07-02 17:00:47 +0300

    Added required functions to control radio

--- Core/Inc/radio.h
+++ Core/Inc/radio.h
@@ -11,8 +11,28 @@
 #include "nrf24.h"
 #include "retarget.h"
 
+struct radio_t
+{
+    struct nrf24_t *nrf_radio;
+    enum nrf24_address_width_t address_width;
+    enum nrf24_air_data_rate_t data_rate;
+    uint8_t data_width;
+    uint8_t channel;
+    bool is_in_rx_mode;
+};
+
+enum radio_operation_result_t
+{
+    RADIO_OK = 0,
+    RADIO_ERROR,
+    RADIO_RETRY,
+};
+
 void radio_raise_irq_flag(void);
-void radio_process_irq(struct nrf24_t *radio);
-void radio_shut_down(struct nrf24_t *radio);
+enum radio_operation_result_t radio_process_irq(struct radio_t *radio);
+enum radio_operation_result_t radio_shut_down(struct radio_t *radio);
+enum radio_operation_result_t radio_receive(struct radio_t *radio, uint8_t *data);
+enum radio_operation_result_t radio_init(struct radio_t *radio);
+enum radio_operation_result_t
 
 #endif /* INC_RADIO_H_ */
